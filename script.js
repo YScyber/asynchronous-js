6b34b93aed8a5c9cf018c8bbc072be6e57236141
YScyber <155051841+YScyber@users.noreply.github.com>
2024-08-02 14:12:35 +0900

    コメントの内容を修正

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // オプションを設定
 const options = {
-    root: null, // ルート要素 (nullはデフォルト値) nullにした場合、見ている画面との交差監視を有効にする
+    root: null, // ルート要素をnull（nullはデフォルト値）にした場合、見ている画面との交差監視を有効にする
     rootMargin: "-50% 0px", // 交差を検知するルート要素からの距離
-    threshold: 0 // 閾値 (0はデフォルト値) 0にした場合、見え始めと見え終わりのときにコールバック関数が呼ばれる
+    threshold: 0 // 閾値を0（0はデフォルト値）にした場合、見え始めと見え終わりのときにコールバック関数が呼ばれる
 };
 
 // IntersectionObserverコンストラクターを呼び出して交差したときに実行したい関数と、オプション設定を渡す
 const observer = new IntersectionObserver(intersectingElement, options);
 
-// 今回の交差を監視するarticle要素
+// 今回の交差を監視する<article>要素
 const articles = document.querySelectorAll("article");
 
-// それぞれのarticle要素を監視する
+// それぞれの<article>要素を監視する
 articles.forEach((article) => {
     observer.observe(article);
 });
